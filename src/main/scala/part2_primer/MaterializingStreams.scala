0d6e2d6c62311e95ff9cd8621ce9504fb387ee5e
Sergio Cano <serdeliverance@gmail.com>
2020-07-05 14:08:11 -0300

    materializing streams - additional notes

--- src/main/scala/part2_primer/MaterializingStreams.scala
+++ src/main/scala/part2_primer/MaterializingStreams.scala
@@ -3,6 +3,7 @@ package part2_primer
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.{Flow, Keep, Sink, Source}
+import playground.MaterializingStreamsReview.system
 
 import scala.util.{Failure, Success}
 
@@ -15,31 +16,56 @@ object MaterializingStreams extends App {
   val simpleGraph = Source(1 to 10).to(Sink.foreach(println))
   // val materializedValue = simpleGraph.run()
 
-  val source = Source(1 to 10)
   // after completing the reduce it exposes a materialized value of type Future[Int]
-  val sink = Sink.reduce[Int]((a, b) => a + b)
+
+  val source = Source(1 to 10)
+  val sink = Sink.reduce[Int]((a,b) => a + b)
   val sumFuture = source.runWith(sink)
+
+  import system.dispatcher
+
   sumFuture.onComplete {
-    case Success(value) => println(s"The sum of all elements is: $value")
-    case Failure(exception) => println(s"The sum of the elements could not be computed: $exception")
+    case Success(value) => println(s"The sum of all elements is $value")
+    case Failure(ex) => println(s"Error. the sum could not be computed: $ex")
   }
 
-  // choosing materialized values
+
+  // choosing value to materialize
+
   val simpleSource = Source(1 to 10)
   val simpleFlow = Flow[Int].map(x => x + 1)
   val simpleSink = Sink.foreach[Int](println)
 
+  // how to choose which value to materialize?
+
+  simpleSource.viaMat(simpleFlow)((sourceMat, flowMat) => flowMat)
+  /*
+    Source>>viaMat recibes a combine function to decide which value to materialize from the
+    two connected components
+   */
+
+  //  simpleSource.viaMat(simpleFlow)((sourceMat, flowMat) => flowMat)
+  simpleSource.viaMat(simpleFlow)(Keep.right)   // it is the same as the line above
+
+  // now we choose to materialize the most right component (simpleSink)
+  // So, when running this graph it will return Future[Done]
   val graph = simpleSource.viaMat(simpleFlow)(Keep.right).toMat(simpleSink)(Keep.right)
+
   graph.run().onComplete {
-    case Success(_) => println("Stream processing finished")
-    case Failure(exception) => println(s"Stream processing failed with: $exception")
+    case Success(_) => println("stream processing finished")
+    case Failure(ex) => println(s"stream processinf failed: $ex")
   }
 
-  // sugars
-  Source(1 to 10).runReduce[Int](_ + _)  // same
+  // syntatic sugars for materializing
+
+  // forward
+  Source(1 to 10).runWith(Sink.reduce[Int](_ + _))  // source.to(Sink.reduce)(Keep.right)
+  Source(1 to 10).reduce(_ + _) // same
 
   // backwards
   Sink.foreach[Int](println).runWith(Source.single(42)) // source(..).to(sink...).run()
+  // source(..).to(sink...) always materialized the left value
+
   // both ways
   Flow[Int].map(x => 2 * x).runWith(simpleSource, simpleSink)
 
